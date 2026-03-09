84982d3caf672507fe30536c4f0e7440450e3bb1
lichang-jin <3517179979@qq.com>
2026-03-09 12:21:48 +0800

    update

--- VGGT-Long/LoopModels/aggregators/salad.py
+++ VGGT-Long/LoopModels/aggregators/salad.py
@@ -1,5 +1,115 @@
+import math
 import torch
 from torch import Tensor
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Salad(nn.Module):
+    def __init__(
+        self,
+        num_channels: int = 1536,
+        num_clusters: int = 64,
+        cluster_dim: int = 128,
+        token_dim: int = 256,
+        dropout: float = 0.3,
+    ) -> None:
+        super().__init__()
+        self.num_channels = num_channels
+        self.num_clusters = num_clusters
+        self.cluster_dim = cluster_dim
+        self.token_dim = token_dim
+
+        if dropout > 0:
+            self.dropout = nn.Dropout(dropout)
+        else:
+            self.dropout = nn.Identity()
+
+        # MLP for global scene token g
+        self.token_features = nn.Sequential(
+            nn.Linear(self.num_channels, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.token_dim),
+        )
+        # MLP for local features f_i
+        self.cluster_features = nn.Sequential(
+            nn.Conv2d(self.num_channels, 512, 1),
+            self.dropout,
+            nn.ReLU(),
+            nn.Conv2d(512, self.cluster_dim, 1),
+        )
+        # MLP for score matrix S
+        self.score_features = nn.Sequential(
+            nn.Conv2d(self.num_channels, 512, 1),
+            self.dropout,
+            nn.ReLU(),
+            nn.Conv2d(512, self.num_clusters, 1),
+        )
+        # Dustbin parameter z
+        self.dust_bin = nn.Parameter(torch.tensor(1.0))
+
+    def forward(self, x) -> Tensor:
+        """
+        Args:
+            x (tuple): A tuple containing two elements, f and t.
+        Returns:
+            f (Tensor): The global descriptor [B, m*l + g]
+        """
+        f, t = x
+        p = self.score_features(f).flatten(2)
+        f = self.cluster_features(f).flatten(2)
+        t = self.token_features(t)
+
+        p = get_matching_probs(p, self.dust_bin, 3)
+        p = torch.exp(p)
+        p = p[:, :-1, :]
+
+        p = p.unsqueeze(1).repeat(1, self.cluster_dim, 1, 1)
+        f = f.unsqueeze(2).repeat(1, 1, self.num_clusters, 1)
+        f = torch.cat([
+            F.normalize(t, p=2, dim=-1),
+            F.normalize(f * p.sum(dim=-1), p=2, dim=1).flatten(1)
+        ], dim=-1)
+        f = F.normalize(f, p=2, dim=-1)
+        return f
+
+
+def log_otp_solver(log_a: Tensor, log_b: Tensor, M: Tensor, num_iters: int = 20, reg: float = 1.0) -> Tensor:
+    """
+    Sinkhorn matrix scaling algorithm for Differentiable Optimal Transport problem.
+    This function solves the optimization problem and returns the OT matrix for the given parameters.
+    Args:
+        log_a: Source weights
+        log_b: Target weights
+        M: metric cost matrix
+        num_iters: The number of iterations.
+        reg: regularization value
+    """
+    M = M / reg  # regularization
+
+    u, v = torch.zeros_like(log_a), torch.zeros_like(log_b)
+
+    for _ in range(num_iters):
+        u = log_a - torch.logsumexp(M + v.unsqueeze(1), dim=2).squeeze()
+        v = log_b - torch.logsumexp(M + u.unsqueeze(2), dim=1).squeeze()
+
+    return M + u.unsqueeze(2) + v.unsqueeze(1)
+
+
+def get_matching_probs(S, dustbin_score = 1.0, num_iters = 3, reg = 1.0):
+    """
+    Sinkhorn algorithm.
+    """
+    batch_size, m, n = S.size()
+
+    # augment scores matrix
+    S_aug = torch.empty(batch_size, m + 1, n, dtype=S.dtype, device=S.device)
+    S_aug[:, :m, :n] = S
+    S_aug[:, m, :] = dustbin_score
+
+    # prepare normalized source and target log-weights
+    norm = -torch.tensor(math.log(n + m), device=S.device)
+    log_a, log_b = norm.expand(m + 1).contiguous(), norm.expand(n).contiguous()
+    log_a[-1] = log_a[-1] + math.log(n-m)
+    log_a, log_b = log_a.expand(batch_size, -1), log_b.expand(batch_size, -1)
+    log_P = log_otp_solver(log_a, log_b, S_aug, num_iters, reg)
+    return log_P - norm
--- VGGT-Long/LoopModels/vpr_model.py
+++ VGGT-Long/LoopModels/vpr_model.py
@@ -59,3 +59,116 @@ class VPRModel(pl.LightningModule):
         self.batch_acc = [] # We will keep track of the % of trivial pairs/triplets at the loss level.
         self.faiss_gpu = faiss_gpu
 
+        self.backbone = utils.get_backbone(backbone_arch, backbone_config, vggt_long_config)
+        self.aggregator = utils.get_aggregator(aggregator_arch, aggregator_config)
+        self.val_outputs = []
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.aggregator(x)
+        return x
+
+    def configure_optimizers(self):
+        if self.optimizer.lower() == "sgd":
+            optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=self.momentum, weight_decay=self.weight_decay)
+        elif self.optimizer.lower() == "adamw":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        else:
+            raise NotImplementedError(f"Optimizer {self.optimizer} not implemented")
+
+        if self.lr_scheduler.lower() == "linear":
+            scheduler = lr_scheduler.LinearLR(
+                optimizer,
+                start_factor=self.lr_scheduler_config['start_factor'],
+                end_factor=self.lr_scheduler_config['end_factor'],
+                total_iters=self.lr_scheduler_config['total_iters']
+            )
+        elif self.lr_scheduler.lower() == "cosine":
+            scheduler = lr_scheduler.CosineAnnealingLR(optimizer, self.lr_scheduler_config['T_max'])
+        elif self.lr_scheduler.lower() == "multistep":
+            scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=self.lr_scheduler_config['milestones'], gamma=self.lr_scheduler_config['gamma'])
+        else:
+            raise NotImplementedError(f"Scheduler {self.lr_scheduler} not implemented")
+
+        return [optimizer], [scheduler]
+
+    def optimizer_step(
+        self,
+        epoch: int,
+        batch_idx: int,
+        optimizer,
+        optimizer_closure = None,
+    ) -> None:
+        optimizer.step(closure=optimizer_closure)
+        self.lr_schedulers().step()
+
+    def loss_function(self, descriptors, labels):
+        if self.miner is not None:
+            miner_output = self.miner(descriptors, labels)
+            loss = self.loss_fn(descriptors, labels, miner_output)
+
+            # Calculate the % of trivial pairs/triplets which do not contribute in the loss value
+            nb_samples = descriptors.shape[0]
+            nb_mined = len(set(miner_output[0].detach().cpu().numpy()))
+            batch_acc = 1.0 - nb_mined / nb_samples
+        else:
+            loss = self.loss_fn(descriptors, labels)
+            batch_acc = 0.0
+            if type(loss) == tuple:
+                loss, batch_acc = loss
+
+        self.batch_acc.append(batch_acc)
+        self.log('b_acc', sum(self.batch_acc) / len(self.batch_acc), prog_bar=True, logger=True)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        places, labels = batch
+        BS, N, ch, H, W = places.shape
+        images = places.view(BS * N, ch, H, W)
+        labels = labels.view(-1)
+
+        descriptors = self(images)
+        if torch.isnan(descriptors).any():
+            raise ValueError("NaN values in descriptors")
+        loss = self.loss_function(descriptors, labels)
+        self.log('loss', loss.item(), prog_bar=True, logger=True)
+        return {'loss': loss}
+
+    def on_train_batch_end(self, outputs, batch, batch_idx: int) -> None:
+        # We empty the batch_acc list for next epoch.
+        self.batch_acc = []
+
+    def validation_step(self, batch, batch_idx, dataloader_idx = None):
+        places, _ = batch
+        descriptors = self(places)
+        self.val_outputs[dataloader_idx].append(descriptors.detach().cpu())
+        return descriptors.detach().cpu()
+
+    def on_validation_epoch_start(self) -> None:
+        # reset the outputs list
+        self.val_outputs = [[] for _ in range(len(self.trainer.datamodule.val_dataloaders))]
+
+    def on_validation_epoch_end(self) -> None:
+        # [R1, R2, ..., Rn, Q1, Q2, ...]
+        val_step_outputs = self.val_outputs
+        dm = self.trainer.datamodule
+        if len(dm.val_datasets) == 1:
+            val_step_outputs = [val_step_outputs]
+
+        for i, (val_set_name, val_dataset) in enumerate(zip(dm.val_set_names, dm.val_datasets)):
+            feats = torch.concat(val_step_outputs[i], dim=0)
+
+            if "pitts" in val_set_name:
+                num_references = val_dataset.dbStruct.numDb
+                positives = val_dataset.getPositives()
+            elif 'msls' in val_set_name:
+                num_references = val_dataset.num_references
+                positives = val_dataset.pIdx
+            else:
+                raise NotImplementedError(f'Please implement validation_epoch_end for {val_set_name}')
+
+            r_list = feats[:num_references]
+            q_list = feats[num_references:]
+            pitts_dict =
+
