61d55d28aac92626aaaebff01d53201aec3b5456
lichang-jin <3517179979@qq.com>
2026-03-10 21:26:48 +0800

    update

--- VGGT-Long/vggt_long.py
+++ VGGT-Long/vggt_long.py
@@ -11,7 +11,6 @@ import datetime
 from PIL import Image
 from pathlib import Path
 from tqdm.auto import tqdm
-
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -33,10 +32,155 @@ from LoopModelDBoW import RetrievalDBow
 from loop_utils import Sim3LoopOptimizer, load_config
 
 def remove_duplicates(data_list):
-    pass
+    """data_list: [(67, (3386, 3406), 48, (2435, 2455)), ...]"""
+    seen = {}
+    result = []
+    for item in data_list:
+        if item[0] == item[2]: continue
+        key = (item[0], item[2])
+        if key not in seen.keys():
+            seen[key] = item
+            result.append(item)
+    return result
+
+def extract_p2_k_matrix(calib_path):
+    """from calib.txt get K  (kitti)"""
+    calib_path = Path(calib_path)
+    if not calib_path.exists():
+        raise FileNotFoundError(f"Calibration file not found: {calib_path}")
+
+    with open(calib_path, 'r') as f:
+        for line in f:
+            if line.startswith('P2:'):
+                values = line.split(':')[1].split()
+                values = [float(v) for v in values]
+                p2_matrix = np.array(values).reshape(3, 4)
+                k_matrix = p2_matrix[:3, :3]
+                return k_matrix, p2_matrix
+
+    raise ValueError("P2 not found in calibration file.")
+
+class LongSeqResult:
+    def __init__(self):
+        self.combined_extrinsics = []
+        self.combined_intrinsics = []
+        self.combined_depth_maps = []
+        self.combined_depth_confs = []
+        self.combined_world_points = []
+        self.combined_world_points_confs = []
+        self.all_camera_poses = []
+        self.all_camera_intrinsics = []
+
 
 class VGGTLong:
     def __init__(self, image_dir, save_dir, config):
         self.config = config
         self.chunk_size = self.config['Model']['chunk_size']
         self.overlap = self.config['Model']['overlap']
+        self.conf_threshold = 1.5
+        self.seed = 42
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.dtype = torch.bfloat16 if torch.cuda.get_device_capability()[0] >= 8 else torch.float16
+        self.sky_mask = False
+        self.useDBoW = self.config['Model']['useDBoW']
+        self.img_dir = image_dir
+        self.img_list = None
+        self.delete_temp_files = self.config['Model']['delete_temp_files']
+        self.out_dir = save_dir
+        self.result_unaligned_dir = os.path.join(save_dir, '_tmp_results_unaligned')
+        self.result_aligned_dir = os.path.join(save_dir, '_tmp_results_aligned')
+        self.result_loop_dir = os.path.join(save_dir, '_tmp_results_loop')
+        self.pcd_dir = os.path.join(save_dir, 'pcd')
+        os.makedirs(self.result_unaligned_dir, exist_ok=True)
+        os.makedirs(self.result_aligned_dir, exist_ok=True)
+        os.makedirs(self.result_loop_dir, exist_ok=True)
+        os.makedirs(self.pcd_dir, exist_ok=True)
+
+        self.all_camera_poses = []
+        self.all_camera_intrinsics = []
+
+        if self.config['Weights']['model'] == 'VGGT':
+            self.model = VGGTAdapter(self.config)
+        elif self.config['Weights']['model'] == 'Pi3':
+            self.model = Pi3Adapter(self.config)
+        elif self.config['Weights']['model'] == 'MapAnything':
+            self.model = MapAnythingAdapter(self.config)
+        else:
+            raise ValueError(f"Unsupported model: {self.config['Weights']['model']}. ")
+
+        self.sky_seg_session = None
+        self.chunk_indices = None   # [(begin_idx, end_idx), ...]
+        self.loop_list = []         # e.g. [(1584, 139), ...]
+        self.loop_optimizer = Sim3LoopOptimizer(self.config)
+        self.sim3_list = []         # [(s [1,], R [3,3], T [3,]), ...]
+        self.loop_sim3_list = []    # [(chunk_idx_a, chunk_idx_b, s [1,], R [3,3], T [3,]), ...]
+        self.loop_predict_list = []
+        self.loop_enable = self.config['Model']['loop_enable']
+
+        if self.loop_enable:
+            if self.useDBoW:
+                self.retrieval = RetrievalDBow(self.config)
+            else:
+                loop_info_save_path = os.path.join(save_dir, "loop_closures.txt")
+                self.loop_detector = LoopDetector(image_dir=image_dir, output=loop_info_save_path, config=self.config)
+        print("init done.")
+
+    def get_loop_pairs(self):
+        if self.useDBoW: # DBoW2
+            for frame_id, img_path in tqdm(enumerate(self.img_list)):
+                image_ori = np.array(Image.open(img_path))
+                if len(image_ori.shape) == 2:
+                    # gray to rgb
+                    image_ori = cv2.cvtColor(image_ori, cv2.COLOR_GRAY2RGB)
+
+                frame = image_ori   # (height, width, 3)
+                frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+                self.retrieval(frame, frame_id)
+                cands = self.retrieval.detect_loop(thresh=self.config['Loop']['DBoW']['thresh'], num_repeat=self.config['Loop']['DBoW']['num_repeat'])
+
+                if cands is not None:
+                    (i, j) = cands # e.g. cands = (812, 67)
+                    self.retrieval.confirm_loop(i, j)
+                    self.retrieval.found.clear()
+                    self.loop_list.append(cands)
+                self.retrieval.save_up_to(frame_id)
+
+        else: # DINOv2
+            self.loop_detector.run()
+            self.loop_list = self.loop_detector.get_loop_list()
+
+    def process_single_chunk(self, range_1, chunk_idx=None, range_2=None, is_loop=False):
+        start_idx, end_idx = range_1
+        chunk_image_paths = self.img_list[start_idx:end_idx]
+        if range_2 is not None:
+            start_idx, end_idx = range_2
+            chunk_image_paths += self.img_list[start_idx:end_idx]
+
+        predictions = self.model.infer_chunk(chunk_image_paths)
+        for key in predictions.keys():
+            if isinstance(predictions[key], torch.Tensor):
+                predictions[key] = predictions[key].cpu().numpy().squeeze(0)
+
+        if is_loop:
+            save_dir = self.result_loop_dir
+            filename = f"loop_{range_1[0]}_{range_1[1]}_{range_2[0]}_{range_2[1]}.npy"
+        else:
+            if chunk_idx is None:
+                raise ValueError("chunk_idx must be provided when is_loop is False")
+            save_dir = self.result_unaligned_dir
+            filename = f"chunk_{chunk_idx}.npy"
+        save_path = os.path.join(save_dir, filename)
+
+        if not is_loop and range_2 is None:
+            extrinsics = predictions['extrinsics']
+            intrinsics = predictions['intrinsics']
+            chunk_range = self.chunk_indices[chunk_idx]
+            self.all_camera_poses.append((chunk_range, extrinsics))
+            self.all_camera_intrinsics.append((chunk_range, intrinsics))
+        predictions['depth'] = np.squeeze(predictions['depth'])
+
+        np.save(save_path, predictions)
+
+        return predictions if is_loop or range_2 is not None else None
+
+    def process_long_sequence(self):
